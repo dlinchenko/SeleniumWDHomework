a2fee34c276f19fc2f73db650a01465b960e9474
dlinchenko <d.linchenjo@outlook.com>
2018-07-24 17:12:37 +0300

    added tv page actions

---
+++ SeleniumWDHomework/PageActions/TVPageActions.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System.Linq;
+
+namespace SeleniumWDHomework.PageActions
+{
+    class TVPageActions
+    {
+        private IWebDriver _driver;
+
+        public TVPageActions(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void ClickOnElement(IWebElement element) => element.Click(); 
+
+        public IWebElement GetItemNameOnGeneralTVPage() => _driver.FindElement(By.CssSelector(".g-i-tile-i-title.clearfix"));
+
+        public IWebElement GetItemNameOnTVProductPage() => _driver.FindElement(By.ClassName("detail-title"));
+
+        public IWebElement GetAplliedFilter(string filter) => _driver.FindElement(By.Id(filter));
+
+        public IWebElement[] GetAllItemsNamesOnGeneralTVPage() => _driver.FindElements(By.CssSelector(".g-i-tile-i-title.clearfix")).ToArray();
+
+
+
+    }
+}
